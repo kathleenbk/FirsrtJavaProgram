b06430ec87f4f3d36579ddbf39678d423d5df207
kathleenbk <katie.kava3@gmail.com>
2022-06-06 09:45:59 -0800

    init commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("My name is Katie Kavanagh");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Bellevue, WA");
+	}
+}
